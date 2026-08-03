d4fe41cb20a64df5ca514ff2b84a9a4137753883
Gerges <gerges245003@gmail.com>
2026-08-03 19:29:38 +0300

    Wire publishing config, module registration, and internal route prefix

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -17,6 +17,7 @@ import { PrismaModule } from "./common/persistence/prisma.module";
 
 import { AppController } from "./app.controller";
 import { StrategyModule } from './modules/strategy/strategy.module';
+import { PublishingModule } from './modules/publishing/publishing.module';
 
 @Module({
   imports: [
@@ -62,6 +63,7 @@ import { StrategyModule } from './modules/strategy/strategy.module';
     JourneyModule,
     MarketingKnowledgeModule,
     StrategyModule,
+    PublishingModule,
   ],
   controllers: [AppController],
 })
